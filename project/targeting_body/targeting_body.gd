94ada96b7760a6274ffae0a60fbcacf03f44bc90
Alex G <alex.gestwicki@gmail.com>
2024-04-15 08:19:14 -0400

    Add orbiting functionality

--- project/targeting_body/targeting_body.gd
+++ project/targeting_body/targeting_body.gd
@@ -2,13 +2,17 @@ class_name TargetingBody
 extends CharacterBody2D
 
 enum TargetType {ENEMY}
+enum MoveMode {ORBIT, CHASE}
+
+const ORBIT_DISTANCE := 50
 
 @export var speed := 200.0
 @export var target_type : TargetType = TargetType.ENEMY
 
 var targeting_area : Area2D
+var move_mode : MoveMode = MoveMode.ORBIT
 var target : Node2D
-var controller : Node
+var controller : Node2D : set = set_controller
 
 
 func _ready()->void:
@@ -22,8 +26,16 @@ func _ready()->void:
 func _physics_process(delta:float)->void:
 	if is_instance_valid(target):
 		var direction := get_angle_to(target.global_position)
+		
+		if move_mode == MoveMode.ORBIT:
+			if distance_sqrd_to(target) < pow(ORBIT_DISTANCE, 2) / 4:
+				direction += PI
+			elif distance_sqrd_to(target) < pow(ORBIT_DISTANCE, 2):
+				direction += PI / 2
+		
 		$PolygonGenerator.rotation = direction
 		move_and_collide(Vector2.RIGHT.rotated(direction) * speed * delta)
+	
 	get_closest_target()
 
 
@@ -32,6 +44,11 @@ func get_closest_target()->void:
 		if is_possible_target(node):
 			if distance_sqrd_to(node) < distance_sqrd_to(target):
 				target = node
+				move_mode = MoveMode.CHASE
+	
+	if not is_instance_valid(target) or target == controller:
+		target = controller
+		move_mode = MoveMode.ORBIT
 
 
 func is_possible_target(node:PhysicsBody2D)->bool:
@@ -46,3 +63,7 @@ func distance_sqrd_to(object:Node2D)->float:
 	if is_instance_valid(object):
 		return global_position.distance_squared_to(object.global_position)
 	return INF
+
+
+func set_controller(value:Node2D)->void:
+	controller = value
