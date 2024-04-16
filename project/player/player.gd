826a16d9a551a92d22990beeaec8038dfffd2209
Alex G <alex.gestwicki@gmail.com>
2024-04-16 08:08:06 -0400

    Add spawning different drone types

--- project/player/player.gd
+++ project/player/player.gd
@@ -8,15 +8,19 @@ var team := 0
 
 
 func _physics_process(delta:float)->void:
-	if Input.is_action_just_pressed("spawn_drone"):
-		spawn_drone()
+	if Input.is_action_just_pressed("repair_drone"):
+		spawn_drone("repair_drone")
+	if Input.is_action_just_pressed("attack_drone"):
+		spawn_drone("attack_drone")
+	if Input.is_action_just_pressed("defense_drone"):
+		pass
 	
 	var movement_vector := Input.get_vector("left", "right", "up", "down")
 	move_and_collide(movement_vector * delta * speed)
 
 
-func spawn_drone()->void:
-	var drone : Drone = preload("res://drones/repair_drone.tscn").instantiate()
+func spawn_drone(dronetype:String)->void:
+	var drone : Drone = load("res://drones/" + dronetype + ".tscn").instantiate()
 	drone.controller = self
 	drone.global_position = global_position
 	drone.set_rings(1, Color.WHITE)
